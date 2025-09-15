48622d7555bd40ff7fb209c4f1fe4a2f891c43c1
YangWiz <polymagicyang@gmail.com>
2025-09-15 14:56:45 +0200

    Align candidate execution with the formal syntax.

--- LeanCats/Data.lean
+++ LeanCats/Data.lean
@@ -4,18 +4,15 @@ inductive Thread : Type where
   | mk: Nat -> Thread
 deriving BEq, Repr, DecidableEq
 
-abbrev write := "write"
-abbrev read := "read"
+inductive Op : Type where
+  | write : Op
+  | read : Op
+  | fence : Op
+  | branch : Op
+deriving BEq, Repr, DecidableEq
 
-/-
-Actions are of several kinds, which we detail in the course of this article. For now, we
-only consider read and write events relative to memory locations. For example, for the
-location x, we can have a read of the value 0 noted Rx = 0, or a write of the value 1,
-noted Wx = 1. We write proc(e) for the thread holding the event e and addr(e) for its
-address, or memory location.
--/
 structure Action : Type where
-  action : String
+  op: Op
   target : String
   -- For read, the value can not be determined at the begining.
   value : Option Nat
@@ -33,55 +30,60 @@ structure Event where
   (a : Action) -- Action performed
 deriving BEq, Repr, DecidableEq
 
+-- Events can be (for brevity this is not an exhaustive list):
+-- writes, gathered in the set W, including the the set IW of initial writes coming from the prelude of the program;
+-- reads, gathered in the set R;
+-- branch events, gathered in the set B;
+-- fences, gathered in the set F.
+structure Events where
+  (W : Set Event)
+  (IW : Set Event)
+  (R : Set Event)
+  (B : Set Event)
+  (F : Set Event)
+
+def Events.empty : Events :=
+  {
+    W := {}
+    IW := {}
+    R := {}
+    B := {}
+    F := {}
+  }
+
+def Events.Inhabited : Inhabited Events :=
+  { default := Events.empty }
+
+-- Each execution is abstracted to a candidate execution 〈evts , po, rf, IW, sr〉 providing
 structure CandidateExecution where
-  (E : List Event)
+  (evts : Events)
   (po : Rel Event Event)
   (rf : Rel Event Event)
-  (co : Rel Event Event)
-
--- User provided data
--- E: List of events [e₁, e₂ ... ]
--- po : [(e₁, e₂), (e₂, e₃) ...]
--- rf : [(e₃, e₄), (e₄, e₅) ...]
--- co : [(e₁, e₂) ...]
+  (IW : Set Event)
+  (sr : Rel Event Event)
 
 def Rel.empty : Rel Event Event := fun _ _ => False
 instance : EmptyCollection (Rel Event Event) := ⟨Rel.empty⟩
 
 namespace CandidateExecution
 
-def empty : CandidateExecution := { E := [], po := Rel.empty, rf := Rel.empty, co := Rel.empty}
+def empty [Inhabited Events] : CandidateExecution :=
+  { evts := default , po := Rel.empty, rf := Rel.empty, sr := Rel.empty, IW := {} }
 
-
-def Events (ce : CandidateExecution) : Type :=
-  @Set.Elem Event {e | e ∈ ce.E}
-
-def coeRel (ce : CandidateExecution) (r : Rel Event Event) :
-  Rel ce.Events ce.Events :=
-  fun x y => r x.val y.val
-
-instance {ce : CandidateExecution} :
-  Coe (Rel Event Event) (Rel ce.Events ce.Events) :=
-  ⟨coeRel ce⟩
-
-structure IsWellFormed (ce : CandidateExecution) where
-  poTotal : IsStrictTotalOrder ce.Events (ce.coeRel ce.po)
-  -- what else?
-
-def mkExecution
-  (E : List Event)
-  (po : List (Event × Event))
-  (rf : List (Event × Event))
-  (co : List (Event × Event))
-  : Option CandidateExecution :=
-  if po.Nodup ∧ rf.Nodup ∧ co.Nodup
-  then
-    let po_rel := λ a b ↦ (a, b) ∈ po
-    let rf_rel := λ a b ↦ (a, b) ∈ rf
-    let co_rel := λ a b ↦ (a, b) ∈ co
-    CandidateExecution.mk E po_rel rf_rel co_rel
-  else
-    none
+-- def Events (ce : CandidateExecution) : Type :=
+--   @Set.Elem Event {e | e ∈ ce.E}
+--
+-- def coeRel (ce : CandidateExecution) (r : Rel Event Event) :
+--   Rel ce.Events ce.Events :=
+--   fun x y => r x.val y.val
+--
+-- instance {ce : CandidateExecution} :
+--   Coe (Rel Event Event) (Rel ce.Events ce.Events) :=
+--   ⟨coeRel ce⟩
+--
+-- structure IsWellFormed (ce : CandidateExecution) where
+--   poTotal : IsStrictTotalOrder ce.Events (ce.coeRel ce.po)
+--   -- what else?
 
 -- We later prove that this constructor makes well-formed CEs
 -- This is not the case right now.
