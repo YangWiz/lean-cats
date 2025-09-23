98ec89e68638a145067d88f9f7b217af0cc368b9
YangWiz <polymagicyang@gmail.com>
2025-09-23 20:52:43 +0200

    add binary and unary ops.

--- LeanCats/Macro.lean
+++ LeanCats/Macro.lean
@@ -6,8 +6,11 @@ namespace Cats
 
 syntax "[expr|" expr "]" : term
 syntax "[keyword|" keyword "]" : term
-syntax "[assertion|" assertion "]" : term
 syntax "[inst|" inst "]" : command
+syntax "[model|" command* "]" : model
+syntax "[assertion|" assertion "]" : term
+syntax "[annotable-events|" annotable_events "]" : term -- Set
+syntax "[predefined-events|" predefined_events "]" : term
 
 macro_rules
   | `([expr| $e₁:expr | $e₂:expr]) => `(Event.union [expr| $e₁] [expr| $e₂])
@@ -16,21 +19,62 @@ macro_rules
   | `([expr| $k:keyword ]) => `([keyword| $k])
 
 macro_rules
-  | `(keyword| co) => `(Event.co)
-  | `(keyword| rf) => `(Event.rf)
-  | `(keyword| fr) => `(Event.fr)
-  | `(keyword| po) => `(Event.po)
-  | `(keyword| W) => `(Event.W)
-  | `(keyword| R) => `(Event.R)
-  | `(keyword| M) => `(Event.M)
-  | `(keyword| emptyset) => `(CandidateExecution.empty)
+  | `([keyword| and]) => Lean.Macro.throwUnsupported
+  | `([keyword| as]) => Lean.Macro.throwUnsupported
+  | `([keyword| begin]) => Lean.Macro.throwUnsupported
+  | `([keyword| call]) => Lean.Macro.throwUnsupported
+  | `([keyword| do]) => Lean.Macro.throwUnsupported
+  | `([keyword| end]) => Lean.Macro.throwUnsupported
+  | `([keyword| enum]) => Lean.Macro.throwUnsupported
+  | `([keyword| flag]) => Lean.Macro.throwUnsupported
+  | `([keyword| forall]) => Lean.Macro.throwUnsupported
+  | `([keyword| from]) => Lean.Macro.throwUnsupported
+  | `([keyword| fun]) => Lean.Macro.throwUnsupported
+  | `([keyword| in]) => Lean.Macro.throwUnsupported
+  | `([keyword| instructions]) => Lean.Macro.throwUnsupported
+  | `([keyword| let]) => Lean.Macro.throwUnsupported
+  | `([keyword| match]) => Lean.Macro.throwUnsupported
+  | `([keyword| procedure]) => Lean.Macro.throwUnsupported
+  | `([keyword| rec]) => Lean.Macro.throwUnsupported
+  | `([keyword| scopes]) => Lean.Macro.throwUnsupported
+  | `([keyword| with]) => Lean.Macro.throwUnsupported
+  | `([keyword| $a:assertion]) => `([assertion| $a])
 
 macro_rules
-  | `([assertion| acyclic]) => `(Acyclic)
   | `([assertion| irreflexive]) => `(Irreflexive)
+  | `([assertion| acyclic]) => `(Acyclic)
   | `([assertion| empty]) => `(IsEmpty)
+
+-- macro_rules
+--   | `([annotable-events| W]) => _
 
 macro_rules
   | `([inst| let $nm = $e]) => `(def $nm := [expr|$e])
   | `([inst| $a:assertion $e as $nm]) => `(def $nm : Prop := [assertion| $a] [expr| $e])
-  | `([inst| (* $_ *)]) => `(#print "")
+  -- | `([inst| (* $_ *)]) => `(#print "")
+
+-- The value of expr 2 must be a set of values S and the operator returns the set S augmented with the value of expr 1.
+-- The cat specification didn't give us the precedence, so we use the Ocaml as the reference.
+infixl:61 " ++ " => Set.insert
+
+-- Cartesian product.
+infixl:61 " * " => Set.prod
+
+infixl:61 " | " => Set.union
+
+infixl:61 " & " => Set.inter
+
+infixl:61 " ; " => Rel.comp
+
+postfix:61 "*" => Relation.ReflTransGen
+
+postfix:61 "+" => Relation.TransGen
+
+postfix:61 "^-1" => Rel.inv
+
+end Cats
+
+-- theorem test123 :
+--   ∀E : CandidateExecution, IsEmpty E.evts.B :=
+--   by
+--     intro E
