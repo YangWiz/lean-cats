8f63b2a3909296709e209ee82b948425e3253dc5
YangWiz <polymagicyang@gmail.com>
2025-10-30 23:51:17 +0100

    Add rf_fr_is_co

--- LeanCats/Theoriems.lean
+++ LeanCats/Theoriems.lean
@@ -48,3 +48,46 @@ lemma internalImpliesPoOrPoMinusOne {e₁ e₂ : Event} (evts : Events) :
         aesop
       }
     }
+
+lemma rf_fr_is_co (evts : Events) (co : Events -> Rel Event Event) (e₁ e₂ e₃ : Event) :
+  (rf.wellformed evts e₁ e₂ ∧ fr evts co e₂ e₃) = co evts e₁ e₃ :=
+  by
+    simp
+    apply Iff.intro
+    {
+      intro hrffr
+      have hrf : rf.wellformed evts e₁ e₂ := by apply And.left hrffr
+      have hfr : fr evts co e₂ e₃ := by apply And.right hrffr
+      simp at hfr
+      have hwunique : e₂.act.op = Op.read -> (∃w, isWrite w ∧ rf evts w e₂) ∧ (∀ w₁ w₂, rf evts w₁ e₂ -> rf evts w₂ e₂ -> w₁ = w₂) :=
+      by
+        exact hrf.wExtAndUnique
+
+      simp at *
+      let ⟨w, hw⟩ := hfr
+      -- At this point, what we want to get is w is e₁.
+      have sameW :
+        (∃ w, w.act.op = Op.write ∧ rf evts w e₂)
+        ∧ ∀ (w₁ w₂ : Event), rf evts w₁ e₂ → rf evts w₂ e₂ → w₁ = w₂ :=
+      by
+        apply hwunique
+        apply hrf.rRead
+      have hwe₁ : (e₁ = w) :=
+      by
+        apply And.right sameW
+        {
+          obtain ⟨hrfin, hunique⟩ := hrf
+          exact hrfin
+        }
+        {
+          obtain ⟨left, mid, right⟩ := hw
+          exact mid
+        }
+
+      obtain ⟨left, mid, right⟩ := hw
+      rw [<-hwe₁] at right
+      exact right
+    }
+    {
+      sorry
+    }
