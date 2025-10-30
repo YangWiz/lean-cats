a40ec23ac5fd87ecc4d6e2807c57e0bf18e19069
YangWiz <polymagicyang@gmail.com>
2025-10-30 23:24:24 +0100

    refactor strict order for rf.

--- LeanCats/Relations.lean
+++ LeanCats/Relations.lean
@@ -5,7 +5,6 @@ open Data
 
 @[simp] def union (r₁ r₂ : Rel Event Event) := λ x y ↦ r₁ x y ∨ r₂ x y
 @[simp] def inter (r₁ r₂ : Rel Event Event) := λ x y ↦ r₁ x y ∧ r₂ x y
-@[simp] def sequence (r₁ r₂ : Rel Event Event) := fun x z ↦ ∃ y, r₁ x y ∧ r₂ y z
 
 -- Not sure if this is the correct definition of cartesian product.
 def prod (s₁ s₂ : Set Event) : Rel Event Event := λ e₁ e₂ ↦ e₁ ∈ s₁ ∧ e₂ ∈ s₂
@@ -52,15 +51,10 @@ instance : Inter (Rel Event Event) := ⟨inter⟩
 @[simp] def Rel.ext (e₁ e₂ : Event) : Prop :=
   e₁.t_id ≠ e₂.t_id
 
-@[simp] def complus (evts : Events) (co : Events -> Rel Event Event) e1 e2 :=
-  Rel.rf evts e1 e2 ∨ co evts e1 e2 ∨ Rel.fr evts co e1 e2 ∨
-  sequence (co evts) (Rel.rf evts) e1 e2 ∨ sequence (Rel.fr evts co) (Rel.rf evts) e1 e2
+@[simp] def isWrite (e : Event) : Prop :=
+  e.act.op = Op.write
 
--- instance PartialOrder: PartialOrder (Rel.rf evts e₁ e₂) :=
---   {
---     le_antisymm := _
---   }
-structure rf (evts : Events) (e₁ e₂ : Event) where
+structure rf (evts : Events) (e₁ e₂ : Event) : Prop where
   -- left one in the evts.
   lIn : e₁ ∈ evts
   rIn : e₂ ∈ evts
@@ -68,12 +62,10 @@ structure rf (evts : Events) (e₁ e₂ : Event) where
   rRead : e₂.act.op = Op.read
   sameTarget : e₁.act.target = e₂.act.target
 
-@[simp] def rf.wellformed (evts : Events) : Rel Event Event :=
-  λ e₁ e₂ ↦ rf evts e₁ e₂
-  ∧ ∀ r : Event, r.act.op = Op.read -> (∃w, rf evts w r)
-  ∧ (∀ w₁ w₂, rf evts w₁ r -> rf evts w₂ r -> w₁ = w₂)
+@[simp] def fr (evts : Events) (co : Events -> Rel Event Event) (e1 e2 : Event) : Prop :=
+  ∃w, isWrite w ∧ rf evts w e1 ∧ co evts w e2
 
-instance rf.strictOrder (evts : Events) : IsStrictOrder Event (rf evts) where
+instance (evts : Events) : IsStrictOrder Event (rf evts) where
   irrefl :=
   by
     intro e
@@ -98,12 +90,22 @@ instance rf.strictOrder (evts : Events) : IsStrictOrder Event (rf evts) where
       rw [abSameTarget]
       rw [bcSameTarget]
 
-    exact {
-      lIn := lIn
-      rIn := rIn
-      lWrite := lWrite
-      rRead := rRead
-      sameTarget := sameTarget
-    }
+    exact ⟨lIn, rIn, lWrite, rRead, sameTarget⟩
+
+-- This defines:
+-- Write event must exists
+-- Write equlity (If the two write events write to the same read event, then these two writes are the same)
+structure rf.wellformed (evts : Events) (e₁ e₂ : Event) extends rf evts e₁ e₂ where
+  wExtAndUnique {r} : r.act.op = Op.read ->
+    (∃w, isWrite w ∧ rf evts w r)
+    ∧ (∀ w₁ w₂, rf evts w₁ r -> rf evts w₂ r -> w₁ = w₂)
+
+def comPlus (evts : Events) (co : Events -> Rel Event Event) (e₁ e₂ : Event) :=
+  rf evts e₁ e₂ ∨ co evts e₁ e₂ ∨ fr evts co e₁ e₂
+  ∨ Relation.Comp (co evts) (rf evts) e₁ e₂ ∨ Relation.Comp (fr evts co) (rf evts) e₁ e₂
+
+def com (evts : Events) (co : Events -> Rel Event Event) (e₁ e₂ : Event) :=
+  rf evts e₁ e₂ ∨ co evts e₁ e₂ ∨ fr evts co e₁ e₂
+
 
 end CatRel
